392feef3021451ba8a60c72a39c7755af0c51a30
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2019-04-02 10:31:19 +0200

    1.Init

---
+++ src/b26_gui_tabla/B26_GUI_tabla.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package b26_gui_tabla;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class B26_GUI_tabla {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
